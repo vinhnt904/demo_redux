e9a14a646cbdc557a126ab07e4394b3f16818c09
TuanVinh904 <tuanvinh904@gmail.com>
2023-02-19 11:48:39 +0700

    add structure

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>ComponentName</Text>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
